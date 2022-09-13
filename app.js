08a421d06905cf10234063349030e744dd8541e2
eko-develops <ekodevelops@gmail.com>
2022-09-13 15:53:09 -0700

    add basic functions

--- app.js
+++ app.js
@@ -7,7 +7,83 @@ const main = () => {
 
 const handleFormSubmit = (e) => {
 	e.preventDefault();
-	console.log('hello');
+	const textArea = document.getElementById('prices');
+
+	const separatedPrices = textArea.value.split('\n');
+
+	const pairs = separatedPrices.map((pair) => pair.split(' '));
+
+	for (let i = 0; i < pairs.length; i++) {
+		pairs[i] = pairs[i].map((string) => parseInt(string));
+	}
+
+	const result = pairs.map(([price, percent]) => {
+		const convertedPercent = percent / 100;
+		const discountAmount = price * convertedPercent;
+		const salePrice = price - discountAmount;
+
+		return {
+			percent,
+			discountAmount,
+			salePrice,
+			regularPrice: price,
+		};
+	});
+
+	appendToOutput(result);
+};
+
+const appendToOutput = (result) => {
+	const output = document.querySelector('#output table');
+
+	const headerRow = document.createElement('tr');
+
+	const headers = createTableHeaders();
+	const tableData = createTableData(result);
+
+	headers.forEach((header) => headerRow.append(header));
+
+	output.append(headerRow);
+	tableData.forEach((table) => output.append(table));
+};
+
+const createTableHeaders = () => {
+	const regPriceHeader = document.createElement('th');
+	regPriceHeader.innerText = 'Regular Price';
+
+	const percentHeader = document.createElement('th');
+	percentHeader.innerText = 'Percent';
+
+	const discountAmountHeader = document.createElement('th');
+	discountAmountHeader.innerText = 'Discount Amount';
+
+	const salePriceHeader = document.createElement('th');
+	salePriceHeader.innerText = 'Sale Price';
+
+	return [regPriceHeader, percentHeader, discountAmountHeader, salePriceHeader];
+};
+
+const createTableData = (result) => {
+	const data = result.map((price) => {
+		console.log(price);
+		const row = document.createElement('tr');
+
+		const discountAmount = document.createElement('td');
+		discountAmount.innerText = price.discountAmount;
+		const regPrice = document.createElement('td');
+		regPrice.innerText = price.regularPrice;
+
+		const percent = document.createElement('td');
+		percent.innerText = price.percent;
+		const salePrice = document.createElement('td');
+		salePrice.innerText = price.salePrice;
+
+		row.append(regPrice, percent, discountAmount, salePrice);
+
+		return row;
+	});
+
+	return data;
 };
 
 main();
